2944e0a4a767dce0139ec2ca0d470e808152f54c
nekosaur <albert@kaaman.nu>
2024-04-07 16:09:52 +0200

    chore: use js extension

--- api/showtimes.ts
+++ api/showtimes.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { ShowtimeModel } from '../shared/showtimes/showtime.schema.ts'
-import '../shared/movies/movie.schema.ts'
-import { withDatabase } from '../shared/db.ts'
+import { ShowtimeModel } from '../shared/showtimes/showtime.schema.js'
+import '../shared/movies/movie.schema.js'
+import { withDatabase } from '../shared/db.js'
 
 export default function (request: VercelRequest, response: VercelResponse) {
   return withDatabase(async () => {
