c0fca30b33ebf5314c337f56ba7aedb47b90585a
nekosaur <albert@kaaman.nu>
2024-04-07 16:34:13 +0200

    feat: try cron job

---
+++ _api/scrape-spegeln.ts
@@ -0,0 +1,13 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  const result = await fetch(
+    'http://worldtimeapi.org/api/timezone/America/Chicago'
+  )
+  const data = await result.json()
+
+  return response.json({ datetime: data.datetime })
+}
